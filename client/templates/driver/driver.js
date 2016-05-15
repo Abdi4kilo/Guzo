b25b012ecba4f396143951cf3703ea3e5a2aa753
Abdi Biru <abdi.g.biru@gmail.com>
2016-05-15 03:10:21 -0400

    find best route is done and sorts by date too

--- client/templates/driver/driver.js
+++ client/templates/driver/driver.js
@@ -3,7 +3,7 @@ Template.driver.events({
 		var destination=document.getElementById('des').value;
 		var departuer=document.getElementById('dep').value;
 		var date=document.getElementById('date').value;
-	
+		var dateTime=new Date(date);
 		Meteor.call('addRoutes',departuer,destination,dateTime);
 	}
 });
--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -3,7 +3,7 @@ if(Meteor.isClient){
 	
 	Template.home.helpers({
 		routes: function(){
-			 return	Routes.find({},{sort:{travelDate:-1}});
+			 return	Routes.find({},{sort:{createdAt:-1}});
 		},
 		session: function(){
 			if(Session.get('Abdi')==="" || Session.get('Abdi')==null){
@@ -19,7 +19,9 @@ if(Meteor.isClient){
 				var distance=[];
 				var destination=document.getElementById('des').value;
 				var departuer=document.getElementById('dep').value;
-				Meteor.call('getShortest',departuer,destination,function(err, closestRoutes) {
+				var date=document.getElementById('date').value;
+				var dateTime=new Date(date);
+				Meteor.call('getShortest',departuer,destination,dateTime,function(err, closestRoutes) {
 					Session.set('Abdi',closestRoutes)
 				});
 		}
